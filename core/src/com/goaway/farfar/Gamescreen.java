54a1564acb025b10f95feec1093b120c9aa9f207
operapay <opera.pay@gmail.com>
2017-11-12 22:37:53 +0700

    move

--- core/src/com/goaway/farfar/Gamescreen.java
+++ core/src/com/goaway/farfar/Gamescreen.java
@@ -1,23 +1,49 @@
 package com.goaway.farfar;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Gamescreen extends ScreenAdapter {
     private GoAwayFarFar goaway;
     private Texture characterImg;
+    private int x;
+    private int y;
     
     public Gamescreen(GoAwayFarFar goaway) {
         this.goaway = goaway;
         characterImg = new Texture("vampire.png");
+        x = 100;
+        y = 100;
     }
     
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    	//x += 5;
+    	update(delta);
         SpriteBatch batch = goaway.batch;
         batch.begin();
-        batch.draw(characterImg, 100, 100);
+        batch.draw(characterImg, x, y);
         batch.end();
+    }
+    
+    private void update(float delta) {
+        if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+            x -= 10;
+        }
+        if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+            x += 10;
+        }
+        if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+            y -= 10;
+        }
+        if(Gdx.input.isKeyPressed(Keys.UP)) {
+            y += 10;
+        }
     }
 }
