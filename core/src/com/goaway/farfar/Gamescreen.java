e8e3c7c601a7d5711103a196bf3c3d7f5e1ca704
operapay <opera.pay@gmail.com>
2017-11-13 11:52:07 +0700

    tridentmove

--- core/src/com/goaway/farfar/Gamescreen.java
+++ core/src/com/goaway/farfar/Gamescreen.java
@@ -13,43 +13,74 @@ public class Gamescreen extends ScreenAdapter {
     private Texture tridentImg;
     private Texture downtridentImg;
     private int x;
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
     private int y;
+    private int speed;
+    private int speed2;
+    
     
     public Gamescreen(GoAwayFarFar goaway) {
         this.goaway = goaway;
         characterImg = new Texture("vampire.png");
         tridentImg = new Texture("trident.png");
         downtridentImg = new Texture("downtrident.png");
-        x = 50;
-        y = 50;
+        x = 0;
+        y = 0;
+        x1 = 100;
+        x2 = 200;
+        y1 = -50;
+        y2 = 450;
+        speed = 2;
+        speed2 = 2;
     }
     
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-    	//x += 5;
+    	y1 += speed;
+        if(y1>=0) {
+        	speed *= -1;
+        	//y1*=-1;
+        }
+        if(y1<-100) {
+        	speed *= -1;
+    	//y1 += 1;
+        }
+    	y2 -= speed2;
+        if(y2<350) {
+        	speed2 *= -1;
+        	//y1*=-1;
+        }
+        if(y2>500) {
+        	speed2 *= -1;
+    	//y1 += 1;
+        }
+    	System.out.println(y2);
     	update(delta);
         SpriteBatch batch = goaway.batch;
         batch.begin();
         batch.draw(characterImg, x, y);
-        batch.draw(tridentImg,100,0);
-        batch.draw(downtridentImg,200,350);
+        batch.draw(tridentImg,100,y1);
+        batch.draw(downtridentImg,200,y2);
         batch.end();
     }
     
     private void update(float delta) {
         if(Gdx.input.isKeyPressed(Keys.LEFT)) {
-            x -= 10;
+            x -= 5;
         }
         if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
-            x += 10;
+            x += 5;
         }
         if(Gdx.input.isKeyPressed(Keys.DOWN)) {
-            y -= 10;
+            y -= 5;
         }
         if(Gdx.input.isKeyPressed(Keys.UP)) {
-            y += 10;
+            y += 5;
         }
     }
 }
